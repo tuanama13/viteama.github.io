2d18a07c218fd7682172af2153818aa49771e219
tuanama13 <tuanama7@gmail.com>
2021-10-11 09:52:04 +0700

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,7 +20,7 @@ const count = ref(0)
 
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
+      Vite Documentation Tes Pages
     </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
